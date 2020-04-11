03a2a00342704ee61dfa5c697c31ce72b251f733
dmdylan <dylandanowski@gmail.com>
2020-04-11 10:32:08 -0400

    Learning unity DOTS

---
+++ Assets/Scripts/Systems/BallMovementSystem.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Jobs;
+using Unity.Physics;
+using UnityEngine;
+
+[AlwaysSynchronizeSystem]
+public class BallMovementSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        float deltaTime = Time.DeltaTime;
+
+        float2 curInput = new float2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        Entities.ForEach((ref PhysicsVelocity vel, in SpeedData speedData) =>
+        {
+            float2 newVel = vel.Linear.xz;
+
+            newVel += curInput * speedData.speed * deltaTime;
+
+            vel.Linear.xz = newVel;
+        }).Run();
+
+        return default;
+    }
+}
---
+++ Assets/Scripts/Systems/MovementSystem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class MovementSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        float deltaTime = Time.DeltaTime;
+
+        Entities.ForEach((ref Translation translation, ref Rotation rotation, in MovementData movementData) =>
+        {
+            if (Input.GetKey(movementData.forwardKey))
+            {
+                translation.Value.z += movementData.movementSpeed * deltaTime;
+            }
+
+            if (Input.GetKey(movementData.backKey))
+            {
+                translation.Value.z -= (movementData.movementSpeed * deltaTime);
+            }
+
+            if (Input.GetKey(movementData.rightKey))
+            {
+                translation.Value.x += movementData.movementSpeed * deltaTime;
+            }
+
+            if (Input.GetKey(movementData.leftKey))
+            {
+                translation.Value.x -= movementData.movementSpeed * deltaTime;
+            }
+
+            rotation.Value = math.mul(rotation.Value, quaternion.RotateX(math.radians(movementData.rotationSpeed * deltaTime)));
+
+        }).Run();
+
+        return default;
+    }
+}
