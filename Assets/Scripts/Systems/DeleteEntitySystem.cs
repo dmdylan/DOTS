26bd642a806c457520b071658129c96cf27e3e9d
dmdylan <dylandanowski@gmail.com>
2020-04-11 19:33:13 -0400

    more systems

---
+++ Assets/Scripts/Systems/DeleteEntitySystem.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Collections;
+
+[AlwaysSynchronizeSystem]
+[UpdateAfter(typeof(PickUpSystem))]
+public class DeleteEntitySystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        EntityCommandBuffer commandBuffer = new EntityCommandBuffer(Allocator.TempJob);
+
+        Entities
+            .WithAll<DeleteTag>()
+            .ForEach((Entity entity) =>
+            {
+                commandBuffer.DestroyEntity(entity);
+            }).Run();
+
+        commandBuffer.Playback(EntityManager);
+        commandBuffer.Dispose();
+
+        return default;
+    }
+}
